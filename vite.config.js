3f44e074e5579f96c9edc534098d5ad209245847
DomsGlobal <domsgloballlp@gmail.com>
2025-08-31 17:12:03 +0530

    tryon

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,6 @@
 import { defineConfig } from 'vite'
 import react from "@vitejs/plugin-react";
 import tailwindcss from '@tailwindcss/vite'
-
 export default defineConfig({
   plugins: [
     tailwindcss(),
@@ -20,29 +19,6 @@ export default defineConfig({
     outDir: "dist"
   }
 })
-
-// import { defineConfig } from 'vite'
-// import react from "@vitejs/plugin-react";
-// import tailwindcss from '@tailwindcss/vite'
-// export default defineConfig({
-//   plugins: [
-//     tailwindcss(),
-//     react()
-//   ],
-//   server: {
-//     proxy: process.env.NODE_ENV === 'development' ? {
-//       "/api": "http://localhost:3001" // Local dev server
-//     } : {}
-//   },
-//   // server: {
-//   //   proxy: {
-//   //     "/api": "http://localhost:5000",
-//   //   },
-//   // },
-//   build: {
-//     outDir: "dist"
-//   }
-// })
 
 
 // import { defineConfig } from "vite";
