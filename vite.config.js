720f63e1f3b66a3498a104588fb674d9b87af56c
DomsGlobal <domsgloballlp@gmail.com>
2025-08-31 14:30:42 +0530

    add all

--- vite.config.js
+++ vite.config.js
@@ -8,9 +8,25 @@
 
 
 import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    react()
   ],
+  build: {
+    outDir: "dist"
+  }
 })
+
+
+// import { defineConfig } from "vite";
+
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: "dist"
+//   }
+// });
